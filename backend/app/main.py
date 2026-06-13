e4433bef104ead4121f73d8ccf65a5d03c4093f1
ElvisValcarcel <elvisnvalcarcel@gmail.com>
2026-06-13 19:51:43 -0400

    S1-011 auth scaffold

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,9 @@
 # boilerplate for main
 
 from fastapi import FastAPI
-from routes.health import router as health_router
+from app.routes.health import router as health_router
+from app.routes.auth import router as auth_router
 
 app = FastAPI()
 app.include_router(health_router)
+app.include_router(auth_router)
