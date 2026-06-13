ff06bdd8caf58cc8bccbaed62059767c899cec52
ElvisValcarcel <elvisnvalcarcel@gmail.com>
2026-06-12 21:39:06 -0400

    Apply Ruff formatting fixes

--- backend/app/routes/health.py
+++ backend/app/routes/health.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-#get request for health returns ok
+
+# get request for health returns ok
 @router.get("/health")
 async def health():
-    return {"status":"ok"}
+    return {"status": "ok"}
