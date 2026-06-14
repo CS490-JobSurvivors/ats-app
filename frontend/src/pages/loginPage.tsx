96122307df82191b808726c23afa81fd1106afca
ElvisValcarcel <elvisnvalcarcel@gmail.com>
2026-06-14 17:44:07 -0400

    S1-010-011: Integration init commit

---
+++ frontend/src/pages/loginPage.tsx
@@ -0,0 +1,62 @@
+import { Button, TextField, Container, Stack } from '@mui/material';
+import { useState } from 'react';
+import {supabase} from '../utils/supabaseClient';
+import {loginApi} from '../api/login';
+
+const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async() => {
+    const { data, error } = await supabase.auth.signInWithPassword({ email, password });
+    if ( error ) {
+        console.log(error.message);
+        return ;
+    };
+    
+    if ( !data.session ) {
+        console.log("No token");
+        return;
+    }
+
+    const userData = await loginApi(data.session.access_token);
+    return userData;
+
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleLogin();
+        }}
+      >
+        <Stack>
+          <TextField
+            required
+            placeholder="Enter Email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            label="email"
+          />
+          <TextField
+            required
+            placeholder="Enter Password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            label="password"
+            type="password"
+          />
+          <Button type="submit" onClick={() => {}}>
+            Login
+          </Button>
+        </Stack>
+      </form>
+    </Container>
+  );
+};
+
+export default LoginPage;
---
+++ frontend/src/pages/signupPage.tsx
@@ -0,0 +1,77 @@
+import { Button, TextField, Container, Stack } from '@mui/material';
+import { useState } from 'react';
+import {supabase} from '../utils/supabaseClient';
+import {signupApi} from '../api/signup';
+
+const SignupPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmedPassword] = useState('');
+
+  const handleSignup = async() => {
+    if (password != confirmPassword) {
+      console.log('Passwords Dont Match');
+      return;
+    }
+
+    const { data, error } = await supabase.auth.signUp({
+        email, password
+    });
+
+    if ( error ) {
+        console.log(error.message);
+        return;
+    }
+
+    if ( !data.session ) {
+        return;
+    }
+
+    const userData = await signupApi(data.session?.access_token);
+    return userData;
+
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSignup();
+        }}
+      >
+        <Stack>
+          <TextField
+            required
+            placeholder="Enter Email"
+            label="email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <TextField
+            required
+            placeholder="Enter Password"
+            label="password"
+            type="password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <TextField
+            required
+            placeholder="Retype Password"
+            label="passwordConfirmation"
+            type="password"
+            onChange={(e) => {
+              setConfirmedPassword(e.target.value);
+            }}
+          />
+          <Button type="submit">Sign up</Button>
+        </Stack>
+      </form>
+    </Container>
+  );
+};
+
+export default SignupPage;
