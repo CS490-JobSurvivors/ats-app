6889a7e40e83a44567d71ea236ffc6a2f7ce3db4
ElvisValcarcel <elvisnvalcarcel@gmail.com>
2026-06-15 00:13:01 -0400

    S1-012: Adjust error to not print to console

--- frontend/src/api/logout.ts
+++ frontend/src/api/logout.ts
@@ -3,7 +3,7 @@ import { supabase } from '../utils/supabaseClient';
 export const logout = async () => {
   const { error } = await supabase.auth.signOut();
   if (error) {
-    console.error('Error logging out:', error);
     throw error;
   }
+  //TODO: add a visible error message if the logout fails
 };
