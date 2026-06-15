c426e8b7973b1967b3c75a4b9519ac98ba85022f
Andrew Kim <andrew@mac.mynetworksettings.com>
2026-06-15 16:45:17 -0400

    S1-018: move dashboard to / route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/" element={<DashboardPage />} />
       </Routes>
     </Router>
   );
