abf3a70b756cf2d33efde2940c434c6e40c5d48d
Andrew Kim <ak2755@njit.edu>
2026-07-12 19:55:55 -0400

    S3-006: fix unused within import

--- frontend/src/test/documentLibraryPage.test.tsx
+++ frontend/src/test/documentLibraryPage.test.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import { fireEvent, render, screen, waitFor, within } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import DocumentLibraryPage from '../pages/documentLibraryPage';
 import { supabase } from '../utils/supabaseClient';
